6cbaab353633b8b8fc0db113bf4982e8ec2a8511
Ashutosh Sathe <2ashutoshbs@gmail.com>
2022-11-08 12:23:36 +0530

    Error fixed but still nothing in camera view

--- src/hnode.cpp
+++ src/hnode.cpp
@@ -448,7 +448,7 @@ void HierarchyNode::render(bool shadowmap) {
     if(draw_triangle) {
         glDrawArrays(GL_TRIANGLES, this->vbo_offset, this->triangle_list.size() * 3);
         error = glGetError();
-        std::cout << "After glDrawArrays drawing " << this->triangle_list.size() << " points from offset " << this->vbo_offset << ", glError = " << error << ", " << glewGetErrorString(error) << "\n"; if(error != 0) exit(0);
+        std::cout << "After glDrawArrays drawing " << this->triangle_list.size() * 3 << " points from offset " << this->vbo_offset << ", glError = " << error << ", " << glewGetErrorString(error) << "\n"; if(error != 0) exit(0);
     }
     if(draw_line) {
         glDrawArrays(GL_LINES, this->vbo_offset + this->triangle_list.size() * 3, this->line_list.size() * 2);
