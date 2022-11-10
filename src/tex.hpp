ef773d55f53ba55b0587ea382a8218239db09adf
Ashutosh Sathe <2ashutoshbs@gmail.com>
2022-11-10 20:33:41 +0530

    Texture loading (cubemap)

---
+++ src/tex.hpp
@@ -0,0 +1,45 @@
+#ifndef __TEX_H__
+#define __TEX_H__
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "stb_image.h"
+GLuint loadCubemap(std::vector<std::string> fnames) {
+    /*
+     * fnames must be in
+     * right, left, top, bottom, front, back order
+     */
+    GLuint ret;
+    glGenTextures(1, &ret);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, ret);
+
+    int width, height, n_channels;
+    for(unsigned int i = 0; i < 6; i++) {
+        unsigned char *data = stbi_load(fnames[i].c_str(), &width, &height, &n_channels, 0);
+        if(data) {
+            glTexImage2D(
+                GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                0, GL_RGB, width, height, 0, GL_RGB,
+                GL_UNSIGNED_BYTE, data
+            );
+            stbi_image_free(data);
+        }
+        else {
+            std::cout << "Cubemap texture failed to load at: " << fnames[i] << "\n";
+        }
+    }
+    // TODO: Use MipMaps
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return ret;
+}
+#endif
